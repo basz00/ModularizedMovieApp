16374af3ee904c768b417efe28db3f54a5299464
Mochammad Basofi <basofi@ask-fast.com>
2018-12-06 08:06:58 +0700

    Details 50% done

--- app/src/main/java/com/baz/movie/movies/details/DetailsActivity.kt
+++ app/src/main/java/com/baz/movie/movies/details/DetailsActivity.kt
@@ -3,15 +3,15 @@ package com.baz.movie.movies.details
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.baz.movie.BaseActivity
 import com.baz.movie.R
 import com.baz.movie.extensions.getViewModel
 import com.baz.movie.extensions.observe
-import com.baz.movie.movies.data.CreditsResult
-import com.baz.movie.movies.data.DetailsResult
-import com.baz.movie.movies.data.VideosResult
+import com.baz.movie.movies.data.*
+import com.baz.movie.util.ImageLoader
+import com.baz.movie.util.ImageUrlHelper
+import kotlinx.android.synthetic.main.activity_details.*
 import javax.inject.Inject
 import kotlin.LazyThreadSafetyMode.NONE
 
@@ -33,11 +33,13 @@ internal class DetailsActivity : BaseActivity() {
     internal lateinit var factory: ViewModelProvider.Factory
 
     private val viewModel by lazy(NONE) { getViewModel<DetailsViewModel>(factory) }
+    private val movieId by lazy(NONE) { intent.getStringExtra(EXTRA_MOVIE_ID) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
         observeViewModel()
+        viewModel.getMovieDetails(movieId)
     }
 
     private fun observeViewModel() {
@@ -49,9 +51,9 @@ internal class DetailsActivity : BaseActivity() {
     private fun observeDetails() {
         viewModel.detailsResultLiveData.observe(this) {
             when (it) {
-                is DetailsResult.Success -> ""
-                is DetailsResult.Progress -> ""
-                is DetailsResult.Failed -> ""
+                is DetailsResult.Success -> onDetailsSuccess(it.details)
+                is DetailsResult.Progress -> onDetailsProgress(it.loading)
+                is DetailsResult.Failed -> onDetailsFailed(it.error)
             }
         }
     }
@@ -59,9 +61,9 @@ internal class DetailsActivity : BaseActivity() {
     private fun observeVideos() {
         viewModel.videosResultLiveData.observe(this) {
             when (it) {
-                is VideosResult.Success -> ""
-                is VideosResult.Progress -> ""
-                is VideosResult.Failed -> ""
+                is VideosResult.Success -> onVideosSuccess(it.videos)
+                is VideosResult.Progress -> onVideosProgress(it.loading)
+                is VideosResult.Failed -> onVideosFailed(it.error)
             }
         }
     }
@@ -69,11 +71,51 @@ internal class DetailsActivity : BaseActivity() {
     private fun observeCredits() {
         viewModel.creditsResultLiveData.observe(this) {
             when (it) {
-                is CreditsResult.Success -> ""
-                is CreditsResult.Progress -> ""
-                is CreditsResult.Failed -> ""
+                is CreditsResult.Success -> onCreditsSuccess(it.credits)
+                is CreditsResult.Progress -> onCreditsProgress(it.loading)
+                is CreditsResult.Failed -> onCreditsFailed(it.error)
             }
         }
     }
 
+    private fun onDetailsSuccess(detailsResponse: DetailsResponse) {
+        detailsOverviewTextView.text = detailsResponse.overview
+        detailsReleaseDateTextView.text = getString(R.string.releaseDate, detailsResponse.releaseDate)
+        detailsRatingTextView.text = getString(R.string.rating, detailsResponse.voteAverage.toString())
+        detailsCollapsingToolbarLayout.title = detailsResponse.title
+        val bannerImageUrl = ImageUrlHelper().generateBannerImageUrl(detailsResponse.backdropPath)
+        ImageLoader().loadImage(detailsBannerImageView, bannerImageUrl)
+    }
+
+    private fun onDetailsProgress(isLoading: Boolean) {
+
+    }
+
+    private fun onDetailsFailed(error: String) {
+
+    }
+
+    private fun onVideosSuccess(videosResponse: VideosResponse) {
+
+    }
+
+    private fun onVideosProgress(isLoading: Boolean) {
+
+    }
+
+    private fun onVideosFailed(error: String) {
+
+    }
+
+    private fun onCreditsSuccess(creditsResponse: CreditsResponse) {
+
+    }
+
+    private fun onCreditsProgress(isLoading: Boolean) {
+
+    }
+
+    private fun onCreditsFailed(error: String) {
+
+    }
 }
--- app/src/main/java/com/baz/movie/util/ImageUrlHelper.kt
+++ app/src/main/java/com/baz/movie/util/ImageUrlHelper.kt
@@ -6,7 +6,10 @@ internal class ImageUrlHelper {
 
         private const val BASE_URL = "https://image.tmdb.org/t/p/"
         private const val PORTRAIT_WIDTH_CODE = "w185"
+        private const val BANNER_WIDTH_CODE = "w342"
     }
 
     fun generatePosterImageUrl(imageUrl: String) = "$BASE_URL$PORTRAIT_WIDTH_CODE$imageUrl"
+
+    fun generateBannerImageUrl(imageUrl: String) = "$BASE_URL$BANNER_WIDTH_CODE$imageUrl"
 }
